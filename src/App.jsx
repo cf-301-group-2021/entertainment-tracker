7c5aa1bcbde5ce01f3d9084e698d7e76cc1bef88
Marquesa Asmussen <marquesa_a@yahoo.com>
2021-09-07 12:47:55 -0700

    add React Router and Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <div>
+          <Switch>
+            <Route exact path="/">
+              Home
+            </Route>
+            <Route path="/search">
+              Search
+            </Route>
+            <Route path="/login">
+              Login
+            </Route>
+            <Route path="/shows">
+              Shows
+            </Route>
+            <Route path="/about">
+              About Us
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Navbar>
+        <Nav.Item>
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/search" className="nav-link">Search</Link>
+          <Link to="/login" className="nav-link">Login</Link>
+          <Link to="/shows" className="nav-link">Shows</Link>
+          <Link to="/about" className="nav-link">About Us</Link>
+        </Nav.Item>
+      </Navbar>
+    );
+  }
+}
+
+export default Header;
