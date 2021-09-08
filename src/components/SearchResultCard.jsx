14b78bc390d7cc2bd421d1fcf5db83e2b7009649
stefrie <sriehle@gmail.com>
2021-09-08 12:02:26 -0700

    fixed eslint/styling rules

--- src/components/SearchResultCard.jsx
+++ src/components/SearchResultCard.jsx
@@ -3,18 +3,18 @@ import Card from "react-bootstrap/Card";
 
 class SearchResultCard extends React.Component {
   render() {
-    return (
-		<div className="searchresultcard">
-			<Card style={{ width: "18rem" }}>
-				<Card.Img variant="top" src={this.props.img} />
-				<Card.Body>
-				<Card.Title>{this.props.title}</Card.Title>
-				<Card.Text>{this.props.description}</Card.Text>
-				</Card.Body>
-			</Card>
-	  	</div>
-    );
-  }
+		return (
+			<div className="searchresultcard">
+				<Card style={{ width: "18rem" }}>
+					<Card.Img variant="top" src={this.props.img} />
+					<Card.Body>
+						<Card.Title>{this.props.title}</Card.Title>
+						<Card.Text>{this.props.description}</Card.Text>
+					</Card.Body>
+				</Card>
+			</div>
+		);
+	}
 }
 
 export default SearchResultCard;
