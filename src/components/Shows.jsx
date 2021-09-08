65b617fc6562b3c05519a767f7596b2c4dfec31e
kason <kbraley9@gmail.com>
2021-09-07 19:02:41 -0700

    feat: Add TV Show card component
    Adds a card for the user's shows

--- src/components/Shows.jsx
+++ src/components/Shows.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import ShowCard from "./ShowCard.jsx";
+
+const tvShow = {
+  title: "Game of thrones",
+  description: "dragons and death",
+  status: "ended",
+  timeZone: "west",
+  time: "11am",
+  nextEpisode: "never",
+  network: "HBO",
+};
 
 class Shows extends React.Component {
   render() {
     return (
       <>
-      Shows Coming Soon
+        <ShowCard tvShow={tvShow} />
       </>
     );
   }
