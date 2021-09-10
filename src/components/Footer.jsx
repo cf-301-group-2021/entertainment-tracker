5158c5ca353d99171a8657388c6e70a4843241f5
stefrie <sriehle@gmail.com>
2021-09-10 00:31:28 -0700

    Update font styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,11 +9,11 @@ class Footer extends React.Component {
       <div className="footer">
         <Navbar className="footernavbar">
           <Nav.Item>
-            <Navbar.Text style={{ marginRight: "30px" }}>
+            <Navbar.Text style={{ marginRight: "30px", fontSize: "20px" }}>
               {"\u00A9 2021 tvRepo, LLC"}
             </Navbar.Text>
           </Nav.Item>
-          <Nav.Item style={{ marginRight: "20px" }}>
+          <Nav.Item style={{ marginRight: "20px", fontSize: "20px" }}>
             <Link to="/about">Contact Us</Link>
           </Nav.Item>
         </Navbar>
