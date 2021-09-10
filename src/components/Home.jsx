66e36a1a8ab3d122d8661f1392bf98f3d0b5ed63
stefrie <sriehle@gmail.com>
2021-09-10 11:07:43 -0700

    Add logo, addl styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,16 +4,17 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home">
+        <img src="../../../images/logo-final-300.png" style={{paddingTop: "50px"}} />
         <section className="welcome">Welcome to tvRepo!</section>
+
         <section className="welcomedesc">
           <p>
             You want to find and watch the very best in television
             entertainment. tvRepo is here to help!
           </p>
+          <p>We started tvRepo to solve what seemed to be a universal issue:</p>
           <p>
-            We started tvRepo to solve what seemed to be a universal issue:
-            remembering all the great shows you&apos;re watching (or waiting to
-            watch)!
+            Remembering all the great shows you&apos;re watching!
           </p>
         </section>
       </div>
